182e800bada5376db9c78c422f64ed9a15d5ef02
Ethan Illingsworth <illingsworth.ethan@gmail.com>
2026-03-04 07:22:35 -0600

    add data fetching

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,21 @@
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./config";
 export class DataObject {
-	get() {}
+	collectionPath = "none";
+	constructor(id) {
+		this.id = id;
+	}
+	async get() {
+		const r = await getDoc(doc(db, this.collectionPath, this.id));
+
+		if (r.exists()) {
+			return r.data();
+		}
+	}
 
 	set() {}
 }
 
-export class Card extends DataObject {}
+export class Card extends DataObject {
+	collectionPath = "cards";
+}
--- src/pages/CardPage.jsx
+++ src/pages/CardPage.jsx
@@ -1,14 +1,26 @@
 import { useParams } from "react-router";
 import { Col, Row } from "../components/Flex";
+import { useEffect, useState } from "react";
+import { Card } from "../firebase/firebase";
 
 export default function CardPage() {
 	const { id } = useParams();
+
+	const [data, setData] = useState({});
+
+	useEffect(() => {
+		const card = new Card(id);
+
+		card.get().then((d) => {
+			setData(d);
+		});
+	}, [id]);
 	return (
 		<>
 			<Row>
 				<img />
 				<Col>
-					<h1>{id}</h1>
+					<h1>{data.name}</h1>
 				</Col>
 			</Row>
 		</>
